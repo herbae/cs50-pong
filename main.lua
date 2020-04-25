c015cfa1de540156c507be66398fff6c8c06b782
Iury Lazoski <lazoski@gmail.com>
2020-04-25 18:50:12 -0300

    sons

--- main.lua
+++ main.lua
@@ -26,11 +26,17 @@ function love.load()
     scoreFont = love.graphics.newFont('font.ttf', 32)
     love.graphics.setFont(smallFont)
 
+    sounds = {
+        ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
+    }
+
     math.randomseed(os.time())
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = false,
+        resizable = true,
         vsync = true
     })
 
@@ -65,6 +71,10 @@ function love.keypressed(key)
     end
 end
 
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
 function love.update(dt)
     if gameState == 'serve' then
         if player1Score == POINTS_TO_WIN then
@@ -87,23 +97,27 @@ function love.update(dt)
         if ball.y < 0 then
             ball.y = 0
             ball.dy = -ball.dy
+            sounds.wall_hit:play()
         end
 
         if ball.y + ball.height > VIRTUAL_HEIGHT then
             ball.y = VIRTUAL_HEIGHT - ball.height
             ball.dy = -ball.dy
+            sounds.wall_hit:play()
         end
 
         if ball:collides(player1) then
             ball.dx = -ball.dx * BALL_SPEED_INCREASE
             ball.x = player1.x + player1.width
             ball.dy = math.min(100, math.max(-100, ball.dy + player1.dy / 5))
+            sounds.paddle_hit:play()
         end
 
         if ball:collides(player2) then
             ball.dx = -ball.dx * BALL_SPEED_INCREASE
             ball.x = player2.x - ball.width
             ball.dy = math.min(100, math.max(-100, ball.dy + player2.dy / 5))
+            sounds.paddle_hit:play()
         end
 
         if ball.x < 0 then
@@ -111,6 +125,7 @@ function love.update(dt)
             player2Score = player2Score + 1
             ball:reset()
             gameState = 'serve'
+            sounds.score:play()
         end
 
         if ball.x > VIRTUAL_WIDTH then
@@ -118,6 +133,7 @@ function love.update(dt)
             player1Score = player1Score + 1
             ball:reset()
             gameState = 'serve'
+            sounds.score:play()
         end
 
         ball:update(dt)
